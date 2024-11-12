858482493eaa3bc7fd0bcf745aa02dfe37763450
ahmedSalah1906 <ahmedsalas.ss.2255@gmail.com>
2024-11-12 23:21:13 +0200

    sss

---
+++ Task 1/Controllers/Employees.Controller.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Task_1.Service.Employees;
+using Task_1.VM;
+
+namespace Task_1.Controllers
+{
+    public class Employees : Controller
+    {
+        IEmployeeService employeeService;
+        public Employees(IEmployeeService employeeService)
+        {
+            this.employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            var AllEmps = employeeService.GetAll();
+
+            return View("AllEmps", AllEmps);
+        }
+        public IActionResult Create()
+        {
+            var Emp = new EmployeeVm();
+
+            return View("Create", Emp);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] EmployeeVm emp)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", emp);
+            }
+            else
+            {
+                employeeService.Create(emp);
+                return RedirectToAction("Index");
+            }
+
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+
+            var emp = employeeService.GetById(id);
+            if (emp != null)
+            {
+                return View("Edit", emp);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public IActionResult Update([FromForm] EmployeeVm emp)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", emp);
+            }
+            else
+            {
+                employeeService.Update(emp);
+                return RedirectToAction("Index");
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+            var emp = employeeService.GetById(id);
+            if (emp != null)
+            {
+                employeeService.Delete(id);
+                return  RedirectToAction("Index");
+            }
+            return NotFound();
+
+
+        }
+    }
+}
--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,5 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Task_1.Models;
+using Task_1.Repository.Billings;
+using Task_1.Repository.Customers;
+using Task_1.Repository.Employees;
+using Task_1.Repository.Items;
+using Task_1.Service.Bills;
+using Task_1.Service.Customers;
+using Task_1.Service.Employees;
+using Task_1.Service.Items;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +17,20 @@ builder.Services.AddControllersWithViews();
 //Adding Db Contection String 
 builder.Services.AddDbContext<BillingContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("cs")));
+#region Reposirory resolve
+builder.Services.AddScoped<IEmployeeRepositry,EmployeeRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IItemReopsitory, ItemRepository>();
+builder.Services.AddScoped<IBillingRepository, BillingRepository>();
+
+#endregion
+#region Service Resolve
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IItemService, ItemService>();
+builder.Services.AddScoped<IBillingService, BillingService>();
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
